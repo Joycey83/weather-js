b31d1f75501dde0f5712ccc4827c7461b89c8666
Joycey <Jcheung8385@gmail.com>
2022-12-22 11:08:04 +0000

    added axios and then created a displayWeatherInfo function. console log it to check to see if we can see london weather data

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,8 @@
+const displayWeatherInfo = function (response) {
+  console.log(response.data);
+};
+
 let apiKey = "5dd071644aff4379355022a20839a99e";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+axios.get(apiUrl).then(displayWeatherInfo);
