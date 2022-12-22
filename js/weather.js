bdfdbdae2410d4fb13ae97250b3448e5af761cc7
Joycey <Jcheung8385@gmail.com>
2022-12-22 13:20:54 +0000

    added new date function on JS so the current day, time will be shown for each searched city

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,28 @@
+const formatDate = function (timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hour}:${minutes}`;
+};
+
 const displayWeatherInfo = function (response) {
   console.log(response.data);
   let cityName = document.querySelector("#city-name");
@@ -5,11 +30,13 @@ const displayWeatherInfo = function (response) {
   let weatherDescription = document.querySelector("#weather-description");
   let humidityNum = document.querySelector("#humidity");
   let windSpeedNum = document.querySelector("#wind-speed");
+  let displayDateTime = document.querySelector("#date");
   cityName.innerHTML = response.data.name;
   tempNum.innerHTML = Math.round(response.data.main.temp);
   weatherDescription.innerHTML = response.data.weather[0].description;
   humidityNum.innerHTML = response.data.main.humidity;
   windSpeedNum.innerHTML = Math.round(response.data.wind.speed);
+  displayDateTime.innerHTML = formatDate(response.data.dt * 1000);
 };
 
 let apiKey = "5dd071644aff4379355022a20839a99e";
