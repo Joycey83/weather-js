6b33e0c5f12baa5fc4aef8d803e57454d344abc2
Joycey <Jcheung8385@gmail.com>
2022-12-22 11:02:24 +0000

    added units in metric to the API Url

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,4 @@
 let apiKey = "5dd071644aff4379355022a20839a99e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
