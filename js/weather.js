e15ba09a4728a0998d91a75135f73525e11ac7a0
Joycey <Jcheung8385@gmail.com>
2022-12-27 14:00:00 +0000

    add a forEach loop so that we can loop through different days for different city weather

--- js/weather.js
+++ js/weather.js
@@ -27,10 +27,11 @@ const displayForecast = function () {
   let weatherForecastElement = document.querySelector("#weather-forecast");
 
   let forecastHTML = `<div class="row">`;
-
-  forecastHTML =
-    forecastHTML +
-    ` 
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` 
 
 <div class="col-auto mb-3">
   <div class="card card-border">
@@ -50,29 +51,8 @@ const displayForecast = function () {
 </div>
 
 `;
+  });
 
-  forecastHTML =
-    forecastHTML +
-    ` 
-  
-  <div class="col-auto mb-3">
-    <div class="card card-border">
-      <div class="card-body">
-        <h3 class="weather-forecast-day">Thu</h3>
-        <img
-          src="https://lighthearted-mousse-3ce7cd.netlify.app/icons/04d.svg"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperature">
-          <span class="weather-temperature-max">10˚|</span
-          ><span class="weather-temperature-min">3˚</span>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  `;
   forecastHTML = forecastHTML + `</div>`;
   weatherForecastElement.innerHTML = forecastHTML;
 };
