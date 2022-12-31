1b094b808df00971a996977ac54d4addd724512b
Joycey <Jcheung8385@gmail.com>
2022-12-31 17:25:16 +0000

    redo the max temp response

--- js/weather.js
+++ js/weather.js
@@ -125,8 +125,7 @@ const displayWeatherInfo = function (response) {
   cityName.innerHTML = response.data.name;
 
   weatherDescription.innerHTML = response.data.weather[0].description;
-  maxiTemp = Math.round(response.data.main.temp_max);
-  maxTemperature.innerHTML = maxiTemp;
+  maxTemperature.innerHTML = Math.round(response.data.main.temp_max);
   humidityNum.innerHTML = response.data.main.humidity;
   windSpeedNum.innerHTML = Math.round(response.data.wind.speed);
   displayDateTime.innerHTML = formatDate(response.data.dt * 1000);
