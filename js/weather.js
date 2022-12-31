550f7077a95594633d45da9f577495d9c6facb70
Joycey <Jcheung8385@gmail.com>
2022-12-31 17:45:01 +0000

    added the weather description for the 6 day weather forecast

--- js/weather.js
+++ js/weather.js
@@ -53,7 +53,7 @@ const displayForecast = function (response) {
         alt=""
         width="100"
       />
-      <h4>Cloudy</h4>
+      <h4>${forecastDay.response.data.weather[0].description}</h4>
       <div class="weather-forecast-temperature">
         <span class="weather-temperature-max">${Math.round(
           forecastDay.temp.max
